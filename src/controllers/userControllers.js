ae8689033759c186e9b8468c2205049938df5311
Igor Bandero <igorbandero@gmail.com>
2023-08-13 08:43:56 -0300

    refactor: deleta sessões antigas dos usuários do banco de dados

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcrypt";
 import { v4 as uuid } from "uuid";
-import { checkUserByCPF, checkUserByEmail, newSession, newUser } from "../repositories/userRepository.js";
+import { checkUserByCPF, checkUserByEmail, checkUserBySession, 
+         deleteSession, newSession, newUser } from "../repositories/userRepository.js";
 
 //#################################################################################################
 
