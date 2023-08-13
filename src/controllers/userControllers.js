90b103250dd6d5b08f46045347e61969dec562a5
Igor Bandero <igorbandero@gmail.com>
2023-08-13 09:33:05 -0300

    fix: mensagens de erro

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -17,17 +17,17 @@ export async function registerUser(req, res){
         // Checar se email já cadastrado
         const emailInUse =  await checkUserByEmail(email);
         if (emailInUse.rowCount !== 0){
-            return res.status(409).send({message: "E-mail já cadastrado no sistema!"});
+            return res.status(409).send("E-mail já cadastrado no sistema!");
         }
         // Checar se cpf já cadastrado
         const cpfInUse =  await checkUserByCPF(cpf);
         if (cpfInUse.rowCount !== 0){
-            return res.status(409).send({message: "CPF já cadastrado no sistema!"});
+            return res.status(409).send("CPF já cadastrado no sistema!");
         }
         // Criptografar a senha e registrar usuário no banco de dados
         const cryptedPassword = bcrypt.hashSync(password, 10);
         const user = await newUser(name, cpf, phone, email, cryptedPassword)
-        res.status(201).send({message: "Usuário registrado com sucesso!"});
+        res.status(201).send("Usuário registrado com sucesso!");
     }
     catch(error){
         res.status(500).send(error.message);
@@ -46,12 +46,12 @@ export async function login(req, res){
         // Verificar se email está cadastrado
         const user = await checkUserByEmail(email);
         if (user.rowCount === 0) {
-            return res.status(401).send({ message: "E-mail não cadastrado!"});
+            return res.status(401).send("E-mail não cadastrado!");
         }
         // Verificar se a senha está correta
         const correctPassword = await bcrypt.compare(password, user.rows[0].password);
         if (!correctPassword){
-            return res.status(401).send({ message: "Senha incorreta!" })
+            return res.status(401).send("Senha incorreta!");
         }
         // Gerar um token e salvar (ou alterar) sessão (token) no banco de dados
         const token = uuid();
